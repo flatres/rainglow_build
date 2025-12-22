68f45e679738a89a9022d91fe7720f9d86e8474f
SD Flatres <sdf@marlboroughcollege.org>
2025-12-22 08:23:31 +0000

    SPA Build  on 2025-12-22 08:23:31

--- src/api/v1/utilities/Tasks/Weather.php
+++ src/api/v1/utilities/Tasks/Weather.php
@@ -1,12 +1,14 @@
 <?php
 require __DIR__ . '/../../../vendor/autoload.php';
 
-define('OWM_KEY', $_ENV["OWM_KEY"]);
+
 
 date_default_timezone_set('Europe/London');
 
 $dotenv = \Dotenv\Dotenv::createImmutable(dirname(__FILE__). '/../../');
 $dotenv->load();
+
+define('OWM_KEY', $_ENV["OWM_KEY"]);
 
 $timestamp = date("Y-m-d H:i:s", time());
 
